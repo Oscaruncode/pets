d4610ec98152fceb99b1ec405e50f9ec600a496c
Oscaruncode <oscarfuentesperezosp@gmail.com>
2024-05-11 11:26:30 -0500

    First commit

---
+++ src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.peluqueriacanina;
+import gui.Principal;
+
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal ();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+}
